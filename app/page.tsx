008902f29a2cf5e1fcba0095f691d27754a5e6ea
captainahmedhabib-collab <captainahmedhabib@gmail.com>
2026-07-19 08:32:22 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
-      <h1>MNCOS-OS Sovereign Vault Active</h1>
-      <p>System Status: Operational</p>
-    </div>
-  );
-}
